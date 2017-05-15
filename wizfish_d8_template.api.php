d88bcc9c0c7a627859b28d1491516d3d364785d4
EKennedy <ekennedy@live.ca>
2017-05-15 15:32:47 -0300

    Add files via upload

---
+++ wizfish_d8_template.api.php
@@ -0,0 +1,12 @@
+/**
+ * Respond to a character being created.
+ *
+ * This hook allows modules to respond whenever a new character is created with the generator.
+ *
+ */
+
+function hook_wizfish_d8_template_character_count() {
+	
+
+
+}
