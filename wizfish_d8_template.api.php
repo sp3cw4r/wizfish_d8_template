80d1a42071ec74f9c32708c344f50a7a559d3610
EKennedy <ekennedy@live.ca>
2017-05-15 15:36:28 -0300

    Update wizfish_d8_template.api.php

--- wizfish_d8_template.api.php
+++ wizfish_d8_template.api.php
@@ -1,12 +1,8 @@
 /**
- * Respond to a character being created.
+ * Respond to something happening.
  *
- * This hook allows modules to respond whenever a new character is created with the generator.
+ * This hook allows modules to respond whenever something amazing happens.
  *
  */
 
-function hook_wizfish_d8_template_character_count() {
-	
-
-
-}
+//function hook_wizfish_d8_template_character_count() {}
